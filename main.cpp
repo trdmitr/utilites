c9557178f5f95fe07b9987b88f3ce335d42698b0
trdmitr <trdmitr@gmail.com>
2017-10-16 16:01:29 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<stdio.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	system ("gm mogrify -format jpeg *.png");
+	system ("del *.png");
+	system("pause");
+	return 0;
+}
